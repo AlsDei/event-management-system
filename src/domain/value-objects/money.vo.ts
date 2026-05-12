a957fd02635cbf5b622b620def213b601f076ec0
Dex <140959059+AlsDei@users.noreply.github.com>
2026-05-12 23:05:20 +0700

    Create money.VO

---
+++ src/domain/value-objects/money.vo.ts
@@ -0,0 +1,34 @@
+export class Money {
+    constructor(
+        private readonly amount: number,
+        private readonly currency: string = 'USD'
+    ) {
+        // Acceptance Criteria: Price cannot be less than zero 
+        if (amount < 0) {
+            throw new Error("Money amount cannot be negative.");
+        }
+    }
+
+    equals(other: Money): boolean {
+        return this.amount === other.amount && this.currency === other.currency;
+    }
+
+    add(other: Money): Money {
+        if (this.currency !== other.currency) throw new Error("Currency mismatch");
+        return new Money(this.amount + other.amount, this.currency);
+    }
+
+    multiply(quantity: number): Money {
+        return new Money(this.amount * quantity, this.currency);
+    }
+    getAmount(): number {
+        return this.amount;
+    }
+
+    getCurrency(): string {
+        return this.currency;
+    }
+
+
+
+}
